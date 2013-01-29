0e2ecd8ec6098ef5e78fa3a4ba5645fd5a04ffc4
jonathan <jonathan@jonathan-laptop.(none)>
2013-01-29 09:46:53 -0600

    Convierte a escala de grises la imagen

--- grises.py
+++ grises.py
@@ -7,22 +7,42 @@ imagenPath = 'kristen.jpg'
 def medidas():
     im = Image.open(imagenPath)
     (ancho, alto) = im.size
-    return ancho,alto
+    return ancho,alto,im
+
+def grises(im,ancho,alto):
+    x = 0
+    y = 0
+    
+    pixeles = im.load()
+
+    for x in range(ancho):
+        for y in range(alto):
+            rgb = pixeles[x,y]
+            r = rgb[0]
+            g = rgb[1]
+            b = rgb[2]
+            
+            promedio = int((r+g+b)/3)
+            gris = (promedio,promedio,promedio)
+
+            im.putpixel((x,y),gris)
+
+    im.save('gris.jpg')
 
 def main():
-    ancho,alto = medidas()
+    ancho,alto,im = medidas()
     pygame.init()
     ventana = pygame.display.set_mode([ancho,alto])
     pygame.display.set_caption('Grises')
-    fondo = pygame.image.load(imagenPath)#carga fondo
+    fondo = pygame.image.load(imagenPath).convert()#carga fondo
     ventana.blit(fondo,(0,0))#agrega imagen a ventana
     pygame.display.flip()#actualiza pantalla
     
+    grises(im,ancho,alto)
+
     while True:
         evento = pygame.event.poll()
         if evento.type == pygame.QUIT:
             break
-
-    
 
 main()
