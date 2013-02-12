f716bc1f9a37e92e931cd93ee172fecae87b7ef9
jonathan <jonathan@jonathan-laptop.(none)>
2013-02-12 01:16:26 -0600

    corregido y listo

--- imagen.py
+++ imagen.py
@@ -5,6 +5,7 @@ from PIL import Image, ImageTk
 import sys
 import random
 import math
+import time
 
 class Filtros:
     
@@ -233,18 +234,35 @@ class Filtros:
         conPix = imCon.load()
         h = [(0,0.2,0), (0.2,0.2,0.2), (0,0.2,0)]
 
-        for x in range(self.h):
-            for y in range(self.w):
-                suma = 0
-                for i in range(3):
-                    for j in range (3):
-                        try:
-                            if x < self.w or y < self.h:
-                                suma += int(pixeles[(x-1)+i,(y-1)+j] * h[j][i])
-                        except IndexError:
-                            suma += 0
+        if self.imActual.mode == 'RGB':
+            for x in range(self.w):
+                for y in range(self.h):
+                    suma = 0
+                    for i in range(3):
+                        for j in range (3):
+                            try:
+                                if x < self.w or y < self.h:
+                                    suma += int(max(pixeles[(x-1)+i,(y-1)+j]) * h[j][i])
+                            except IndexError:
+                                suma += 0
                     #print suma
                     conPix[x,y] = suma
+
+
+        if self.imActual.mode == 'L':
+            for x in range(self.w):
+                for y in range(self.h):
+                    suma = 0
+                    for i in range(3):
+                        for j in range (3):
+                            try:
+                                if x < self.w or y < self.h:
+                                    suma += int(pixeles[(x-1)+i,(y-1)+j] * h[j][i])
+                            except IndexError:
+                                suma += 0
+                    #print suma
+                    conPix[x,y] = suma
+
         imCon.save('convolusion.png')
         self.imActual = imCon
         self.actualizarFondo()
@@ -252,6 +270,7 @@ class Filtros:
 
     def bordes(self):
         '''Aplica mascara Sobel para obtener los bordes de una imagen '''
+        tiempoIn = time.time()
         if self.imActual.mode == 'RGB':
             self.binarizar()
         
@@ -288,6 +307,8 @@ class Filtros:
                 elif suma < 0:
                     borPix[x,y] = 0
         imBor.save('bordes.png')
+        tiempoFin = time.time()
+        print 'Mascara Sobel tardo: ', tiempoFin - tiempoIn, ' segundos'
         self.imActual = imBor
         self.actualizarFondo()
                         
