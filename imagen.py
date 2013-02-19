aea1a6e3de024659f0dcfa070d2044c0182209a7
jonathan <jonathan@jonathan-laptop.(none)>
2013-02-18 18:14:44 -0600

    bfs y etiqueta objetos

--- imagen.py
+++ imagen.py
@@ -20,30 +20,33 @@ class Filtros:
         '''Canvas imagen '''
         self.fondo = ImageTk.PhotoImage(self.im)
         self.canvas = Canvas(self.root, width=self.w, height = self.h)
-        #self.canvas.create_image(self.w/2, self.h/2, self.image=fondo)
         self.label = Label(self.canvas, image=self.fondo)
         self.label.image=self.fondo
         self.label.pack()
 
         '''Canvas para los botones '''
-        self.botones = Canvas(self.root, width=self.w, height = 30)
+        self.botones = Canvas(self.root, width=150, height = self.h)
         self.boton = Button(self.botones, text='Escala de grises', fg='black', command=self.grises)
         self.panel = self.botones.create_window(5,0, anchor='nw', window=self.boton)
         self.boton = Button(self.botones, text='Difuminar', fg='black', command=self.difuminar)
-        self.panel = self.botones.create_window(140,0, anchor='nw', window=self.boton)
+        self.panel = self.botones.create_window(5,30, anchor='nw', window=self.boton)
         self.boton = Button(self.botones, text='Binarizar', fg='black', command=self.binarizar)
-        self.panel = self.botones.create_window(240,0, anchor='nw', window=self.boton)
+        self.panel = self.botones.create_window(5,60, anchor='nw', window=self.boton)
         self.boton = Button(self.botones, text='Ruido', fg='black', command=self.salPimienta)
-        self.panel = self.botones.create_window(330,0, anchor='nw', window=self.boton)
+        self.panel = self.botones.create_window(5,90, anchor='nw', window=self.boton)
         self.boton = Button(self.botones, text='Limpiar', fg='black', command=self.quitarSalPimienta)
-        self.panel = self.botones.create_window(400,0, anchor='nw', window=self.boton)
+        self.panel = self.botones.create_window(5,120, anchor='nw', window=self.boton)
         self.boton = Button(self.botones, text='Convolucion', fg='black', command=self.convolucion)
-        self.panel = self.botones.create_window(480,0, anchor='nw', window=self.boton)
+        self.panel = self.botones.create_window(5,150, anchor='nw', window=self.boton)
         self.boton = Button(self.botones, text='Bordes', fg='black', command=self.bordes)
-        self.panel = self.botones.create_window(590,0, anchor='nw', window=self.boton)
+        self.panel = self.botones.create_window(5,180, anchor='nw', window=self.boton)
+        self.boton = Button(self.botones, text='Etiquetar', fg='black', command=self.etiquetar)
+        self.panel = self.botones.create_window(5,210, anchor='nw', window=self.boton)
+        self.boton = Button(self.botones, text='Normalizar', fg='black', command=self.normalizar)
+        self.panel = self.botones.create_window(5,250, anchor='nw', window=self.boton)
 
-        self.canvas.pack(side='top')
-        self.botones.pack(side='bottom')
+        self.botones.pack(side=LEFT)
+        self.canvas.pack()
 
     def imRGB(self):
         '''Abre imagen original y convierte a RGB
@@ -126,21 +129,15 @@ class Filtros:
         
     def binarizar(self):
         pixeles = self.imActual.load()
-        imBinaria = Image.new("L", (self.w, self.h))
+        imBinaria = Image.new('RGB', (self.w, self.h))
         binPix = imBinaria.load()
         
         for x in range(self.w):
             for y in range(self.h):
-                if self.imActual.mode == 'RGB':
-                    if max( pixeles[x,y] ) >= 127:
-                        binPix[x,y] = 255
-                    else:
-                        binPix[x,y] = 0
-                elif self.imActual.mode == 'L':
-                    if pixeles[x,y] >= 127:
-                        binPix[x,y] = 127
-                    else:
-                        binPix[x,y] = 0
+                if pixeles[x,y] >= 30:
+                    binPix[x,y] = (255,255,255)
+                else:
+                    binPix[x,y] = (0,0,0)
         imBinaria.save('binaria.png')
         self.imActual = imBinaria
         self.actualizarFondo()
@@ -273,54 +270,140 @@ class Filtros:
 
 
     def bordes(self):
-        '''Aplica mascara Sobel para obtener los bordes de una imagen '''
+        '''Aplica mascara para obtener los bordes de una imagen '''
         tiempoIn = time.time()
-        if self.imActual.mode == 'RGB':
-            self.binarizar()
         
         pixeles = self.imActual.load()
         imBor = Image.new("L", (self.w, self.h))
         borPix = imBor.load()
 
-        gx = ([-1,0,1], [-2,0,2], [-1,0,1])
-        gy = (([1,2,1], [0,0,0], [-1,-2,-1]))
-
+        g = ([0,1,0], [1,-4,1], [0,1,0])
 
         for y in range(self.h):
             for x in range(self.w):
-                sumaX = 0
-                sumaY = 0
-                for i in range(len( gx[0] )):
-                    for j in range(len( gy[0] )):
+                suma = 0
+                for i in range( x-1, x+2):
+                    for j in range(y-1, y+2):
                         try:
-                            resX = gx[i][j] * pixeles[x+j,y+i]
-                            resY = gy[i][j] * pixeles[x+j,y+i]
-                        except IndexError:
-                            resX = 0
-                            resY = 0
+                            suma += g[i-(x-1)] [j-(y-1)] * pixeles[i,j]
+                        except:
+                            pass
                         
-                        sumaX = resX + sumaX
-                        sumaY = resY + sumaY
-                cuadradosX = pow(sumaX,2)
-                cuadradosY = pow(sumaY,2)
-                g = int(math.sqrt(cuadradosX + cuadradosY))
-                suma = g
-
-                if suma > 255:
-                    borPix[x,y] = 255
-                elif suma < 0:
-                    borPix[x,y] = 0
+                        
+                borPix[x,y]=suma
         imBor.save('bordes.png')
         tiempoFin = time.time()
-        print 'Mascara Sobel tardo: ', tiempoFin - tiempoIn, ' segundos'
+        print 'Mascara tardo: ', tiempoFin - tiempoIn, ' segundos'
         self.imActual = imBor
         self.actualizarFondo()
-                        
+
+
+    def normalizar(self):
+        imNorm = Image.new('L',(self.w,self.h))
+        normPix = imNorm.load()
+        pixeles = self.imActual.load()
+
+        pix = []
+        for x in range(self.w):
+            for y in range(self.h):
+                pix.append( pixeles[x,y])
+
+        maximo = max(pix)
+        minimo = min(pix)
+        prop = 256.0 / (maximo - minimo)
+
+        for x in range(self.w):
+            for y in range(self.h):
+                normPix[x,y]= int(math.floor((pixeles[x,y]-minimo )*prop))
+        imNorm.save('Normalizada.png')
+        self.imActual = imNorm
+        self.actualizarFondo()
+
+
+    def bfs(self,im,color,origen):
+        '''actual = pixel actual
+        total = cuenta el total de pixeles de la figura
+        '''
+        pixeles = im.load()
+        cola = []
+        coordX = []
+        coordY = []
+        cola.append(origen)
+        inicio = pixeles[origen]
+        total = 0
+
+        while len(cola) > 0:
+            x, y = cola.pop(0)
+            actual = pixeles[x,y]
+
+            if actual == inicio or actual == color:
+                for dx in [-1,0,1]:
+                    for dy in [-1,0,1]:
+                        fila,colum = x+dx, y+dy
+                        try:
+                            candidato = pixeles[fila,colum]
+                            if candidato == inicio:
+                                pixeles[fila,colum] = color
+                                total +=1
+                                cola.append((fila,colum))
+                                coordX.append(fila)
+                                coordY.append(colum)
+                        except:
+                            pass
+        return total, coordX, coordY
+
+
+    def etiquetar(self):
+        im = self.imActual.copy()
+        pixeles = im.load()
+        totPix = self.w * self.h
+        porcentajes = []
+        centros = []
+        
+        for x in range(self.w):
+            for y in range(self.h):
+                if pixeles[x,y] == (0,0,0):
+                    r,g,b = random.randint(0,255), random.randint(0,255), random.randint(0,255)
+                    color = (r,g,b)
+                    pixFigura,cX, cY = self.bfs(im, color, (x,y))
+                    porciento = float(pixFigura)/totPix
+                    if porciento > .001:
+                        porcentajes.append([porciento,color])
+                        c = sum(cX)/len(cX), sum(cY)/len(cY)
+                        centros.append(c)
+
+        fondo = porcentajes.index(max(porcentajes))
+        colorFondo = porcentajes[fondo][1]
+
+        fig = 1
+        for i in porcentajes:
+            print 'Figura %s porcentaje de la imagen: %.4f'%(fig,i[0])
+            fig+=1
+
+        #cambia fondo a color gris
+        for x in range(self.w):
+            for y in range(self.h):
+                if pixeles[x,y] == colorFondo:
+                    pixeles[x,y] = (190,190,190)
+
+        #centros de masa
+        for i in centros:
+            pixeles[i]=(0,255,0)
+
+        im.save('final.png')
+        self.imActual = im
+        self.actualizarFondo()
+        
+        #etiqueta centros
+        etiqueta=1
+        for c in centros:
+            i,j = c[0],c[1]
+            Label(self.canvas, text='Obj %d'%etiqueta).place(x=i, y=j)
+            etiqueta+=1
 
 def main():
     try:
         imPath = sys.argv[1]
-        #print imPath
     except:
         print "Selecciona una imagen"
         return
